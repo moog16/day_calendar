39575f8d5d8203471d91257db50f4c772a304591
mgoo <mgoo@switchfly.com>
2015-01-26 22:02:28 -0800

    created array with endtimes sorted

--- script.js
+++ script.js
@@ -6,17 +6,6 @@ function layOutDay(events) {
   renderAppointments(calendar, events)
 }
 
-function renderAppointments(calendar, events) {
-  for(var i=0; i<events.length; i++) {
-    var e = events[i];
-    var elem = createDiv('appointment');
-
-    elem.style.height = e.end - e.start + 'px';
-
-    calendar.getElementsByClassName('calendar-day-layout')[0].appendChild(elem);
-  }
-}
-
 function renderCalendar(calendar) {
   calendar.appendChild(createDiv('calendar-day-layout'));
   
@@ -30,7 +19,7 @@ function renderTimes(calendar) {
   var dayLayout = calendar.getElementsByClassName('calendar-day-layout')[0];
 
   calendar.insertBefore(createDiv('calendar-day-times'), calendar.firstChild);
-  
+
   for(var i=0; i<=dayLength/30; i++) {
     var elem = createDiv('calendar-day-time');
     var minutes = dayBegin + i*30;
@@ -52,6 +41,37 @@ function createDiv(className) {
 }
 
 
-var events = [ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
+function renderAppointments(calendar, events) {
+  var endTimesSorted = events.map(function(e, i, events) {
+    return e.end;
+  }).sort();
+  console.log(endTimesSorted);
+
+  for(var i=0; i<events.length; i++) {
+    var e = events[i];
+    var elem = createDiv('appointment');
+    var endTime = 0;
+
+
+    elem.style.height = e.end - e.start + 'px';
+
+
+    elem.style.top = e.start + 'px';
+
+    calendar.getElementsByClassName('calendar-day-layout')[0].appendChild(elem);
+  }
+}
+
+
+// var events = [ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
+var events = [ 
+{start: 560, end: 620},
+{start: 540, end: 600}, 
+{start: 30, end: 150},
+{start: 90, end: 150}, 
+{start: 610, end: 670} ];
 
 layOutDay(events);
+
+
+// can I assume that the events will be in chronological order?
