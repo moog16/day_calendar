0e42b7f1b61f08b2a3b926b591740393c7a82bed
mgoo <mgoo@switchfly.com>
2015-02-02 01:04:26 -0800

    alphabitized

--- event.js
+++ event.js
@@ -2,13 +2,12 @@ function CalEvent(start, end, id, a) {
   this.id = id;
   this.start = start;
   this.end = end;
-  this.a = a;
+  this.a = a; // delete
   this.top = start;
   this.height = end - start;
   this.elem = createDiv('calendar-event');
   this.overlappingEvents = [];
-  this.maxOverlaps;
-  this.isSet = false;
+  this.maxOverlaps = [];
   this.width;
   this.position;
   this.left;
@@ -19,30 +18,8 @@ CalEvent.prototype.createAndSetPosition = function(calendar, W) {
   elem.style.height = this.height + 'px';
   elem.style.top = this.top + 'px';
   elem.style.left = this.left + 'px';
-  // elem.style.width = this.width(W) + 'px';
+  elem.style.width = this.width + 'px';
   calendar.getElementsByClassName('calendar-day-layout')[0].appendChild(elem);
-}
-
-CalEvent.prototype.isOverlapping = function(otherEvent) {
-  var start = this.start;
-  var otherStart = otherEvent.start;
-  if(start >= otherStart && start <= otherEvent.end ||
-     otherStart >= start && otherStart <= this.end) {
-    return true;
-  }
-}
-
-// have the calEvent be aware of the other overlapping events 
-// by the otherEvents into an array
-CalEvent.prototype.setOverlappingEvents = function(calendarEvents) {
-  for(var i=0; i<calendarEvents.length; i++) {
-    var otherEvent = calendarEvents[i];
-    if(otherEvent.id === this.id) {
-      continue;
-    } else if(this.isOverlapping(otherEvent)) {
-      this.overlappingEvents.push(otherEvent);
-    }
-  }
 }
 
 CalEvent.prototype._getSpotsTaken = function(W) {
@@ -62,15 +39,12 @@ CalEvent.prototype._getSpotsTaken = function(W) {
   return spotsTaken;
 }
 
-CalEvent.prototype.setWidth = function(W) {
-  var spotsTaken = this._getSpotsTaken(W);
-  var remainingRowWidth = spotsTaken.lengthRemaining;
-  var remainingColumns = spotsTaken.remaining
-  for(var i=0; i<this.maxOverlaps.length; i++) {
-    var evt = this.maxOverlaps[i];
-    if(!evt.width) {
-      evt.width = remainingRowWidth/remainingColumns.length;
-    }
+CalEvent.prototype.isOverlapping = function(otherEvent) {
+  var start = this.start;
+  var otherStart = otherEvent.start;
+  if(start >= otherStart && start <= otherEvent.end ||
+     otherStart >= start && otherStart <= this.end) {
+    return true;
   }
 }
 
@@ -81,6 +55,19 @@ CalEvent.prototype.isOverlappingAll = function(overlaps) {
     }
   }
   return true;
+}
+
+// have the calEvent be aware of the other overlapping events 
+// by the otherEvents into an array
+CalEvent.prototype.setOverlappingEvents = function(calendarEvents) {
+  for(var i=0; i<calendarEvents.length; i++) {
+    var otherEvent = calendarEvents[i];
+    if(otherEvent.id === this.id) {
+      continue;
+    } else if(this.isOverlapping(otherEvent)) {
+      this.overlappingEvents.push(otherEvent);
+    }
+  }
 }
 
 CalEvent.prototype.setMaxOverlaps = function() {
@@ -97,4 +84,16 @@ CalEvent.prototype.setMaxOverlaps = function() {
     return overlaps;
   }
   this.maxOverlaps = commonOverlaps([calEvent], calEvent.overlappingEvents);
+}
+
+CalEvent.prototype.setWidth = function(W) {
+  var spotsTaken = this._getSpotsTaken(W);
+  var remainingRowWidth = spotsTaken.lengthRemaining;
+  var remainingColumns = spotsTaken.remaining
+  for(var i=0; i<this.maxOverlaps.length; i++) {
+    var evt = this.maxOverlaps[i];
+    if(!evt.width) {
+      evt.width = remainingRowWidth/remainingColumns.length;
+    }
+  }
 }
